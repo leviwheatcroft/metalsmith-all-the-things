c457b19bb72775c75f536b5d1ce6b42e37646c79
Levi Wheatcroft <levi@wheatcrofts.com.au>
2017-05-27 06:25:56 +0800

    removed dependency, removed published api key

--- deploy.js
+++ deploy.js
@@ -1,11 +1,5 @@
 import deploy from './lib/deploy'
 
-// aws: {
-//   bucketUrl: 'leviwheatcroft.com.s3-website-ap-southeast-2.amazonaws.com',
-//   AWS_ACCESS_KEY_ID: 'AKIAIX6YAAQLV6U2SPHQ',
-//   AWS_SECRET_ACCESS_KEY: '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'
-// },
-
 deploy({
   srcPath: 'build',
   destPath: '',
